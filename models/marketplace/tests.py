1246d02f78f653c15299676007067a6ec3f3c931
ArnimJ <arnimj@outlook.com>
2017-02-25 17:58:09 -0500

    messed something up in the earlier commit

--- models/marketplace/tests.py
+++ models/marketplace/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.conf.urls import include, url
+from django.contrib import admin
+from .models import Textbook, TextbookPost
+from django.views.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from django import forms
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
+from django.utils.dateparse import parse_date
+from django.forms.models import model_to_dict
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+
 
 # Create your tests here.
+class textbookTests(TestCase):
+    fixtures = ["db.json"]
+
+    def success_response(self):
+        # assumes user with id 1 is stored in db
+        response = self.client.get(reverse('getTextbooks', kwargs={'user_id': 1}))
+
+        # checks that response contains parameter id & implicitly
+        # checks that the HTTP status code is 200
+        self.assertContains(response, 'id')
+
+    # user_id not given in url, so error
+    def fails_invalid(self):
+        response = self.client.get(reverse(''))
+        self.assertEquals(response.status_code, 404)
--- models/models/urls.py
+++ models/models/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from ..marketplace.models import Textbook, TextbookPost
-from ..marketplace import views
+from marketplace.models import Textbook, TextbookPost
+from marketplace import views
 
 
 urlpatterns = [
@@ -24,10 +24,10 @@ urlpatterns = [
     url(r'^$', views.index, name='main'),
 
     #API urls
-    url(r'^v1/api/textbooks/$', views.getTextbooks, name="allTextbooks"),
+    url(r'^v1/api/textbooks/$', views.getTextbooks, name="allTextbooks"),     #read
+    url(r'^v1/api/textbooks/delete/$', views.deleteTextbook, name="deleteBook"), #delete
+    url(r'^v1/api/textbooks/create/$', views.createTextbook, name="createTextbook"), #create
+    url(r'^v1/api/textbooks/updateTextbook/$', views.updateTextbook, name="updateTextbook"), #update
     url(r'^v1/api/postTextbookByForm/', views.postTextbookByForm, name="postTextbook"),
-    url(r'^v1/api/textbooklistings/', views.getTextbookPost,  name="getTextbooks"),
-    url(r'^v1/api/textbooks/delete/$', views.deleteTextbook, name="deleteBook"),
-    url(r'^v1/api/textbooks/create/$', views.createTextbook, name="createTextbook"),
-    url(r'^v1/api/textbooks/updateTextbook/$', views.updateTextbook, name="updateTextbook"),
+    url(r'^v1/api/textbooklistings/', views.getTextbookPost, name="getTextbooks"),
 ]
