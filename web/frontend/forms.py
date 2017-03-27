e8661474223601991cb59363a193a43cdfb9b114
ec3bd <ec3bd@virginia.edu>
2017-03-27 01:08:01 -0400

    label fixing

--- web/frontend/forms.py
+++ web/frontend/forms.py
@@ -2,12 +2,13 @@ from django import forms
 
 class SignupForm(forms.Form):
     username = forms.CharField(label='Username', max_length=20)
-    password = forms.PasswordInput()
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), label="Password")
     email = forms.EmailInput()
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username', max_length=20)
-    password = forms.PasswordInput()
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), label="Password")
+
 
 class NewListingForm(forms.Form):
     posttitle = forms.CharField(label='Title', max_length=70)
