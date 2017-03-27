e6f5fdd3fb0c3e6be7853320ee51b153b1d425b2
ec3bd <ec3bd@virginia.edu>
2017-03-26 21:07:00 -0400

    more incrementals

--- exp/middleman/views.py
+++ exp/middleman/views.py
@@ -2,22 +2,28 @@ import urllib.parse
 import urllib.request
 import json
 from django.http import HttpResponse, JsonResponse
+import requests
+
+MODELS = 'http://models-api:8000/v1/api/'
 
 def getTextbooks(request):
     if request.method == 'GET':
         data = urllib.parse.urlencode(dict(request.GET))
-        req = urllib.request.Request('http://models-api:8000/v1/api/textbooks/?' + data)
+        req = urllib.request.Request(MODELS + 'textbooks/?' + data)
         resp_json = urllib.request.urlopen(req).read().decode('utf-8')
         return JsonResponse(json.loads(resp_json))
 
 def getPopularPosts(request):
-    req = urllib.request.Request('http://models-api:8000/v1/api/popularListings/')
+    req = urllib.request.Request(MODELS + 'popularListings/')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     return JsonResponse(json.loads(resp_json))
 
 def getRecentPosts(request):
-    req = urllib.request.Request('http://models-api:8000/v1/api/recentListings/')
+    req = urllib.request.Request(MODELS + 'recentListings/')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     return JsonResponse(json.loads(resp_json))
 
 def createUser(request):
+    data = request.POST
+    resp_json = requests.post(MODELS + 'createUser/')
+    return JsonResponse(json.loads(resp_json))
